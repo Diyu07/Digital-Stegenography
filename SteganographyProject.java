d8059a339b440ea17f4d43c174f1dbded986d68d
Diyu07 <160271211+Diyu07@users.noreply.github.com>
2024-05-29 15:21:03 +0530

    Create SteganographyProject.java

---
+++ SteganographyProject.java
@@ -0,0 +1,24 @@
+SteganographyProject.java
+package steganographyproject;
+
+public class SteganographyProject {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
+
+
+JPEGCodec.java
+
+package steganographyproject;
+
+import java.io.BufferedOutputStream;
+
+class JPEGCodec {
+
+    static ComposePage.JPEGImageEncoder createJPEGEncoder(BufferedOutputStream out) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
